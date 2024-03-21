5cc99da03aaa0472ce305b64c5792e9eca1a409b
Mehedi Hasan <mhedih32@gmail.com>
2024-03-21 11:54:06 +0600

    router content added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello From React Router!</div>,
+  },
+  {
+    path: '/about',
+    element: <div>I am in the about page</div>
+  },
+  {
+    path: '/contact',
+    element: <div>Call me right now</div>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
