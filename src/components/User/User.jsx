5ce36b9e378383bc9ca8c672549c3c065e49d9ae
Mehedi Hasan <mhedih32@gmail.com>
2024-03-22 10:31:37 +0600

    users content added

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,9 +1,14 @@
-
+// import './User.css'
 const User = ({user}) => {
     const {id,name, phone, username} =user;
+    const userStyle ={
+        border:'2px solid green',
+        padding: '10px',
+        borderRadius:'20px'
+    }
     return (
-        <div>
-            <h3>{id}</h3>
+        <div style={userStyle}>
+            {/* <h3>{id}</h3> */}
             <h2>{name}</h2>
             <h3>{phone}</h3>
             <h3>{username}</h3>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,16 +1,16 @@
 import { useLoaderData } from "react-router-dom";
 import User from "../User/User";
+import './Users.css'
 
 const Users = () => {
 
     const users= useLoaderData();
     console.log(users);
-
     return (
         <div>
             <h2>Our Users :{users.length}</h2>
             <h4>Hlw man whats app</h4>
-            <div>
+            <div className="userStyle">
                 {
                     users.map(user => <User key={user.id} user={user}></User>)
                 }
